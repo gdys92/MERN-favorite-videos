3af22b7b0fcc5192d49c274ac304d70c9bc3641e
gdys92 <gdys92@gmail.com>
2021-08-18 10:19:49 -0300

    nav bar

--- react-frontend/src/components/Navbar/Navbar.tsx
+++ react-frontend/src/components/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/new-video">
-                Create a New Video
+                Create a New Vide
               </Link>
             </li>
           </ul>
