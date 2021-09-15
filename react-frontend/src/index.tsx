dc0aee3a333f1687fbcce09fc564d7572e6fb5ba
gdys92 <gdys92@gmail.com>
2021-09-15 11:40:39 -0300

    index2

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -30,7 +30,7 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
